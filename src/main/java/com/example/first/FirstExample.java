006fb66ef7d0e15c59aad924133e1169579d714c
mchinnala <mchinnala@SHVLSP2012F1L.SLS.ads.valuelabs.net>
2019-01-08 11:43:19 +0530

    Converted Java to maven project

---
+++ src/main/java/com/example/first/FirstExample.java
@@ -0,0 +1,10 @@
+package com.example.first;
+
+public class FirstExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Example");
+	}
+
+}
